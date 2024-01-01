a713b04e3dc6791b6c7a10e7975e36a791c78aae
andreSantos1975 <andredev1975@gmail.com>
2024-01-01 20:14:25 -0300

    skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,171 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Title from './Title';
 
 export default function Skills() {
+  const skillsData = [
+    {
+      name: "Facial",
+      amount: 75
+    },
+    {
+      name: "Skin",
+      amount: 34
+    },
+    {
+      name: "SPA",
+      amount: 50
+    },
+    {
+      name: "Cosmetic",
+      amount: 68
+    },
+    {
+      name: "Medical",
+      amount: 57
+    },
+  ];
+
   return (
-    <div>Skills</div>
+    <Section id="skills">
+      <Title value='skills' />
+      <div className="skills__title">
+        <p>Our Skills</p>
+        <h2>Checker Our Supper Skills</h2>
+      </div>
+      <div className="skills">
+        <div className="skills__bars">
+          {
+           skillsData.map(({ name, amount }) => {
+            return (
+              <div className="skills__bar__bar" key={name}>
+                <progress className="progress-bar" value={amount} max="100" />
+                <div className="label-container">
+                  <span>{name}</span>
+                </div>
+                <h3>{amount}%</h3>
+              </div>
+            );
+          })
+          }
+        </div>
+        <div className="skills__content">
+          <p className="title">
+            Beauty Servicess and Products
+          </p>
+          <p className="descriptions">
+            To be a successFull becauty you sochoud have witch range.
+          </p>
+        </div>
+      </div>
+    </Section>
   )
 }
+
+const Section = styled.section`
+   min-height: 100vh;
+   height: 140vh;
+   background-color: var(--secundary-color);
+
+.sideTitle {
+    h1 {
+      color: #fff;
+      font-size: 9rem;
+      z-index: 2;
+    }
+}
+.skills__title {
+    padding: 6rem 10rem;
+     p {
+      text-transform: uppercase;
+      letter-spacing: 0.2rem;
+      color: var(--primary-color);
+     }
+     h2 {
+      color: green;
+      font-size: 2rem;
+     }
+}
+.skills__bars {
+  display: flex;
+  margin-left: 56px;
+  gap: 2rem; /* Ajuste conforme necessário para o espaçamento entre as barras */
+  align-items: center;
+
+  .skills__bar__bar {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+
+    .progress-bar {
+      transform: rotate(-90deg);
+      transform-origin: bottom;
+      width: 10rem;
+      height: 1rem;
+      -webkit-appearance: none;
+      appearance: none;
+    
+      &::-webkit-progress-bar {
+        height: 1rem;
+        background-color: var(--primary-color);
+        border-radius: 20px; /* Estilizando o background da barra de progresso */
+      }
+    
+      &::-webkit-progress-value {
+        height: 1rem;
+        background-color: var(--secondary-color); /* Cor da barra de progresso preenchida */
+        border-radius: 20px; /* Estilizando as extremidades da barra de progresso preenchida */
+      }
+    }
+    
+
+    .label-container {
+      display: flex;
+      flex-direction: column-reverse;
+      align-items: center;
+      gap: 1rem;
+      span {
+        margin-left: 93px;
+        writing-mode: vertical;
+      }
+    
+    }
+    h3 {
+      margin-top: 27px;
+      margin-left: 10px;
+      font-size: 2rem;
+    }
+  }
+}
+}
+.skills__content {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  .title {
+    margin-top: 5rem; 
+    margin-left: 12rem; 
+    font-size: 2rem; 
+    font-weight: bold; 
+    color: var(--primary-color); 
+  }
+
+  .descriptions {
+    margin-top: 1rem; 
+    margin-left: 12rem; 
+    font-size: 1.5rem; 
+    color: var(--secundary-color);
+  }
+  &__content {
+    display: flex;
+    flex-directio: column;
+    gap: 2rem;
+    color: #fff;
+    z-index: 2; 
+  }
+}
+
+
+`;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,146 @@
-import React from 'react'
+import React, { useState } from 'react';
+import loadmore from '../asserts/icon2.ico';
+import styled from 'styled-components';
 
 export default function Testimonials() {
+
+  const [selected, setSelected] = useState(0);
+  const testimonials = [
+    {
+      designation: 'SPA',
+      name: 'Julia Beatriz',
+      review: 'After I started using gerard cannabis I no longer feel back pain. Im going to buy five so I dont run out of them in my stock. ',
+    },
+    {
+      designation: 'FULL',
+      name: 'Flavio Antônio',
+      review: 'Feel back pain. Im going to by ffive so I dont run out of then in my stock.',
+    },
+    {
+      designation: 'SAIN',
+      name: 'Lotus Marquense',
+      review: 'Cannabis I no longer feel back pain. Im going to buy five so I dont run out of them in my stock.  them in my stock. ',
+    },
+  ]
   return (
-    <div>Testimonials</div>
+    <Section>
+      <div className="container">
+        <div className="testimonials">
+          {
+            testimonials.map(({ designation, name, review }, index) => {
+              return (
+                <div className={`testimonials ${selected === index ? "active" : "hidden"}`} key={index}>
+                  <div className="image">
+                    <div className="cicle2">
+                    <img src={loadmore} className='img__icon2' alt='Loadmore ' />
+                    </div>
+                  </div>
+                  <div className="title-container">
+                    <span className="designation">
+                      {designation}
+                    </span>
+                    <h3 className='title'>{name}</h3>
+                  </div>
+                  <p className="descripion">{review}</p>
+                </div>
+              );
+            })
+          }
+        </div>
+        <div className="controls">
+          <button
+            className={selected === 0 ? "active" : ""}
+            onClick={() =>
+              setSelected(0)}>
+          </button>
+          <button
+            className={selected === 1 ? "active" : ""}
+            onClick={() =>
+              setSelected(1)}>
+          </button>
+          <button
+            className={selected === 2 ? "active" : ""}
+            onClick={() =>
+              setSelected(2)}>
+          </button>
+        </div>
+      </div>
+    </Section>
   )
 }
+
+const Section = styled.section`
+  overflow: hidden;
+
+  .container {
+    min-height: 100vh;
+    background-color: var(--primary-color);
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center; /* Centralizar na direção transversal (cross-axis) */
+    gap: 2rem;
+
+    .testimonials {
+      display: flex;
+      gap: 1rem;
+      text-align: center;
+      justify-content: center;
+      width: 50%;
+      margin: 0 auto; 
+      .testimonials {
+        display: flex;
+        flex-direction: column;
+        align-items: center; /* Centralizar na direção transversal (cross-axis) */
+        gap: 1rem;
+        .image {
+         position: relative;
+         .cicle2 {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 6rem;
+          width: 6rem;
+          border-radius: 10rem;
+          background-color: #fff;
+          .img__icon2 {
+            height: 3rem;
+            width: 3rem;
+          }
+         }
+        }
+      }
+      .hidden {
+        display: none;
+      }
+      color: #ffff;
+      .descripion {
+        height: 8rem;
+      }
+    }
+    .controls {
+      display: flex;
+      gap: 1rem;
+
+      button {
+        width: 20px; /* Ajuste o tamanho desejado */
+        height: 20px; /* Ajuste o tamanho desejado */
+        border-radius: 50%;
+        background-color: #fff; /* Cor de fundo */
+        border: 1px solid #fff; /* Borda */
+        cursor: pointer;
+        outline: none;
+        transition: background-color 0.3s ease;
+
+        &.active {
+          background-color: var(--primary-color); /* Cor de fundo quando ativo */
+        }
+
+        &:hover {
+          background-color: #ccc; /* Cor de fundo ao passar o mouse */
+        }
+      }
+    }
+  }
+`;
+
